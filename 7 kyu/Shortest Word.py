a4b3cffd7abfcaa2653536554f377b4ac8cd3085
s4mgoh <s4mgoh.official@gmail.com>
2025-02-07 17:04:03 +0800

    Create Shortest Word.py

---
+++ 7 kyu/Shortest Word.py
@@ -0,0 +1,8 @@
+"""
+Description:
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+"""
+
+def find_short(s):
+    return min(len(word) for word in s.split(' '))
