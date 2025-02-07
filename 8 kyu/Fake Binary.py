fa124ec3d279bce9f4e76eddf008f61f0b932f7d
s4mgoh <s4mgoh.official@gmail.com>
2025-02-07 16:43:10 +0800

    Update Fake Binary.py

--- 8 kyu/Fake Binary.py
+++ 8 kyu/Fake Binary.py
@@ -3,7 +3,8 @@ Description:
 Given a string of digits, you should replace any digit below 5 with '0' 
 and any digit 5 and above with '1'. Return the resulting string.
 
-Note: input will never be an empty string
+Note:
+Input will never be an empty string
 """
 
 def fake_bin(x):
