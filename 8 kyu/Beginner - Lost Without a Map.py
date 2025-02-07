169a2e62b1af3235cbcc7f8066b32e357bfb14ff
s4mgoh <s4mgoh.official@gmail.com>
2025-02-07 16:56:56 +0800

    Create Beginner - Lost Without a Map.py

---
+++ 8 kyu/Beginner - Lost Without a Map.py
@@ -0,0 +1,10 @@
+"""
+Description:
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+[1, 2, 3] --> [2, 4, 6]
+"""
+
+def maps(a):
+    return [digit*2 for digit in a]
