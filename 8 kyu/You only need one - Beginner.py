f758e8d6698b4355e2a953a891ec3b5c14d5f726
s4mgoh <s4mgoh.official@gmail.com>
2025-02-07 16:48:48 +0800

    Create You only need one - Beginner.py

---
+++ 8 kyu/You only need one - Beginner.py
@@ -0,0 +1,11 @@
+"""
+Description:
+You will be given an array a and a value x. All you need to 
+do is check whether the provided array contains the value.
+
+a can contain numbers or strings. x can be either.
+Return true if the array contains the value, false if not.
+"""
+
+def check(seq, elem):
+    return elem in seq
