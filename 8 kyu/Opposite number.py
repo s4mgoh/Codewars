e91e50e5b9e43da05783aa6f6b4d63e49f55c9fe
s4mgoh <196891961+s4mgoh@users.noreply.github.com>
2025-02-07 23:53:39 +0800

    Create Opposite number.py

---
+++ 8 kyu/Opposite number.py
@@ -0,0 +1,13 @@
+"""
+Description:
+Very simple, given a number (integer / decimal / both depending on the language), 
+find its opposite (additive inverse).
+
+Examples:
+1: -1
+14: -14
+-34: 34
+"""
+
+def opposite(number):
+    return -number
