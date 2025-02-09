1e7bab3d3c07bcaf310a3ededed18bf92a7888d8
s4mgoh <196891961+s4mgoh@users.noreply.github.com>
2025-02-09 15:19:07 +0800

    Create Double Char.py
    Signed-off-by: s4mgoh <196891961+s4mgoh@users.noreply.github.com>

---
+++ 8 kyu/Double Char.py
@@ -0,0 +1,12 @@
+"""
+Description:
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+"""
+
+def double_char(s):
+    return "".join(2 * letter for letter in s)
